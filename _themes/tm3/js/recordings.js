1558710e7e7b67dec555fcb29780bc65c0305f95
Joe Dakroub - Webfaction <joe.dakroub@me.com>
2014-08-05 16:55:01 +0000

    Auto commit

--- _themes/tm3/js/recordings.js
+++ _themes/tm3/js/recordings.js
@@ -289,21 +289,22 @@ document.body.addEventListener('click', function(event) {
 
     if (!el.hasClass('nav-button')) return;
 
-    var headingEl = document.getElementById(el.getAttribute('href', 1).replace('#', '')),
-        headingTop = headingEl.getPosition()[1],
-        headingHeight = parseInt(headingEl.querySelector('h2').getStyle('height'));
+    var heading = document.getElementById(el.getAttribute('href', 1).replace('#', '')),
+        y = document.getElementById(el.getAttribute('href', 1).replace('#', '')).getPosition()[1];
 
-    document.body.removeClass('fadein');
-    document.body.addEventListener('transitionend', function transitionend() {
-        document.body.removeEventListener('transitionend', transitionend);
-
-        window.scrollTo(0, window.getDocumentHeight());
-        setTimeout(function() {
-            window.scrollTo(0, document.getElementById(el.getAttribute('href', 1).replace('#', '')).getPosition()[1]);
-        }, 10);
-
-        document.body.addClass('fadein');
-    });
+    if (window.getScreenSize > 2) {
+        document.body.removeClass('fadein');
+        document.body.addEventListener('transitionend', function transitionend() {
+            document.body.removeEventListener('transitionend', transitionend);
+            window.scrollTo(0, window.getDocumentHeight());
+            setTimeout(function() {
+                window.scrollTo(0, y);
+            }, 10);
+            document.body.addClass('fadein');
+        });
+    } else {
+        scroller.scrollTo(0, -y, 1000, IScroll.utils.ease.elastic);
+    }
 
     updateBackgroundImages();
 });
