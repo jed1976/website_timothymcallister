87486069a02a59d45897c1123d80c79aa10cfa16
Joe Dakroub - Webfaction <joe.dakroub@me.com>
2014-08-16 03:55:01 +0000

    Auto commit

--- _themes/tm3/js/tm3.js
+++ _themes/tm3/js/tm3.js
@@ -167,7 +167,7 @@ window.queryHTML = function(html, selector) {
 window.toggleLogoOpacity = function() {
     var y = Math.abs(this.y) || window.pageYOffset,
         logo = document.getElementById('logo'),
-        targetY = window.getDocumentHeight() / 2,
+        targetY = 1000,
         opacity = (100 - ((targetY - y) / targetY) * 100) / 100,
         changeOpacity = function(opacity) {
             logo.style.backgroundColor = 'rgba(0, 0, 0, ' + opacity + ')';
@@ -177,8 +177,6 @@ window.toggleLogoOpacity = function() {
         changeOpacity(1.0);
         return;
     }
-
-    //if (opacity > 0.9 || opacity < 0.2) return;
 
     changeOpacity(opacity);
 };
