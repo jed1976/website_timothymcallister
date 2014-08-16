18f6697d437c397882dd10ac2d4b7e537c8369bf
Joe Dakroub - Webfaction <joe.dakroub@me.com>
2014-08-16 20:55:02 +0000

    Auto commit

--- _themes/tm3/js/home.js
+++ _themes/tm3/js/home.js
@@ -1,16 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var resizeSection = function() {
-        if (window.getScreenSize() < 2) return;
-
-        var logo = document.getElementById('logo'),
-            logoHeight = parseInt(logo.getStyle('height'));
-
-        document.getElementById('live-recordings').style.height = window.getWindowSize() >= 1 ? (window.innerHeight - logoHeight) + 'px' : 'auto';
-    };
-
-    window.addEventListener('resize', function load() {
-//        resizeSection();
-    });
-
-  //  resizeSection();
+    map.addClass('fadein');
 });
