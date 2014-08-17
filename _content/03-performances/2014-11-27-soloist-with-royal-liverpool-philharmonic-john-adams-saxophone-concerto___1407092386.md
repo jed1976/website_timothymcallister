30fcecefa0fb908829aabedc529e229fb4aa0aae
Joe Dakroub <joe.dakroub@me.com>
2014-08-17 16:08:42 -0400

    Updated timestamp for perf

--- _content/03-performances/2014-11-27-soloist-with-royal-liverpool-philharmonic-john-adams-saxophone-concerto___1407092386.md
+++ _content/03-performances/2014-11-27-soloist-with-royal-liverpool-philharmonic-john-adams-saxophone-concerto___1407092386.md
@@ -1,11 +1,11 @@
 ---
 title: 'Soloist with Royal Liverpool Philharmonic; John Adams' Saxophone Concerto'
-event_time: 07:29 PM
+event_time: 07:30 PM
 location: Philharmonic Hall
 latitude: 53.3995957
 longitude: -2.9710726
 performance_url: http://www.liverpoolphil.com/14303/events-classical-music/sax-in-the-city.html
-ticket_information_url: 
-ensemble: 
-conductor: 
+ticket_information_url:
+ensemble:
+conductor:
 ---
---
+++ offline.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+
+<title>Site Offline</title>
+
+<style type="text/css">
+
+body { 
+background-color:	#111; 
+margin:				50px 30px 50px 30px; 
+font-family:		Verdana, Arial, Tahoma, Trebuchet MS, Sans-serif;
+font-size:			11px;
+color:				#fff;
+background-color:	#111;
+}
+#content  {
+padding:			22px 15px 25px 15px;
+border:				rgba(255, 255, 255, 0.1) 1px solid;
+text-align: center;
+}
+</style>
+
+</head>
+
+<body>
+
+<div id="content">
+
+<p>This site will be offline momentarily while we perform some updates.</p>
+<p>We apologize for any inconvenience.</p>
+
+</div>
+
+</body>
+
+</html>
